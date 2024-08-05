d2d141c552711eee1b60b0cc5f59388d59365357
william <lflower1206@gmail.com>
2024-08-05 22:00:47 +0800

    chore: project init

---
+++ lint-staged.config.mjs
@@ -0,0 +1,14 @@
+import { quote } from "shell-quote";
+
+export default {
+  "*.{ts,tsx}": (filenames) => {
+    const escapedFileNames = filenames
+      .map((filename) => `${quote([filename])}`)
+      .join(" ");
+    return [
+      `prettier --write ${escapedFileNames}`,
+      `eslint --fix ${filenames.map((f) => `"${f}"`).join(" ")}`,
+      `git add ${escapedFileNames}`,
+    ];
+  },
+};
